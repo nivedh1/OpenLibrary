9ca3ae8b40e737e820d35b7d854cec2b902067f3
rikeshsharma <rikesh@iitk.ac.in>
2019-07-04 01:32:20 +0530

    mailing feature added xz

--- routes/mailing.js
+++ routes/mailing.js
@@ -32,12 +32,47 @@ router.get("/request/:id", function(req, res) {
                 subject:foundBook.subject,
                 name:req.user.name,
                 email:req.user.email,
-                phone:req.user.Phn_no,
+                phone:req.user.number,
+                user_id:req.user._id,
+                book_id:foundBook._id,
             },
         };
             sgMail.send(msg);
             res.send(`${req.user.email}   ${foundBook.CurrentEmail}`)
         }
+    });
+
+});
+router.get("/reject/:id/:requestorId", function(req, res) {
+    Book.findOne({_id:req.params.id},function(err,foundBook){
+        if(err){
+            console.log(err);
+        }
+        else{
+            user.findById(req.params.requestorId,(err,foundUser)=>{
+                    const msg = {
+                    to:foundUser.email,
+                    from:'openlib@openlib.com',
+                    subject: 'Rejection of requested book ',
+                    text: 'Do not reply',
+                    html:'<h3>Do not reply ,this is an auto generated mail</h3>',
+                    templateId:'bb3d532d-3b08-4cbc-84e6-f98703710336',
+                    substitutions:{
+                        bookName:foundBook.name,
+                        author:foundBook.author,
+                        method:foundBook.lend,
+                        edition:foundBook.edition,
+                        genre:foundBook.genre,
+                        subject:foundBook.subject,
+                        name:req.user.name,
+                        email:req.user.email,
+                        phone:req.user.number,
+                    },
+                };
+                    sgMail.send(msg);
+                    res.send(`${req.user.email}   ${foundBook.CurrentEmail}`)
+            });
+                }
     });
 
 });
